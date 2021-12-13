91073ecda33a372e0f932b99eb33470200bc76e2
Parvez <parvez.mhp@gmail.com>
2021-12-12 19:18:04 -0500

    Publishing the project files

---
+++ src/test/java/page_objects/RealAPR.java
@@ -0,0 +1,68 @@
+package page_objects;
+
+import command_providers.ActOn;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class RealAPR extends NavigationBar {
+    private final By CalculatorTab = By.xpath("//label[text()='Calculator']");
+    private final By HomePriceInputField = By.name("HomeValue");
+    private final By DownPaymentInDollar = By.name("DownPaymentSel");
+    private final By DownPaymentInputField = By.name("DownPayment");
+    private final By InterestRateInputField = By.name("Interest");
+    private final By CalculateRateButton = By.name("calculate");
+    private final By ActualAPRRate = By.xpath("//*[@id='analysisDiv']/table[1]/tbody/tr[6]/td[1]/strong");
+
+    private static final Logger LOGGER = LogManager.getLogger(RealAPR.class);
+
+    public WebDriver driver;
+
+    public RealAPR(WebDriver driver) {
+        super(driver);
+    }
+
+    public RealAPR waitForPageToLoad() {
+        LOGGER.debug("Wait for the page to load");
+        ActOn.wait(driver, CalculatorTab).waitForElementToBeVisible();
+        return this;
+    }
+    public RealAPR typeHousePrice(String value) {
+        LOGGER.debug("Typing the House price: " + value);
+        ActOn.elements(driver, HomePriceInputField).setValue(value);
+        return this;
+    }
+
+    public RealAPR clickOnDownPaymentInDollar() {
+        LOGGER.debug("Clicking on the Down Payment in Dollar");
+        ActOn.elements(driver, DownPaymentInDollar).click();
+        return this;
+    }
+
+    public RealAPR typeDownPayment(String value) {
+        LOGGER.debug("Typing the Down Payment: " + value);
+        ActOn.elements(driver, DownPaymentInputField).setValue(value);
+        return this;
+    }
+
+    public RealAPR typeInterestRate(String value) {
+        LOGGER.debug("Typing the Interest Rate: " + value);
+        ActOn.elements(driver, InterestRateInputField).setValue(value);
+        return this;
+    }
+
+    public RealAPR clickOnTheCalculateButton() {
+        LOGGER.debug("Clicking on the calculate button");
+        ActOn.elements(driver, CalculateRateButton).click();
+        return this;
+    }
+
+    public RealAPR validateActualRealAPR(String expectedAPRrate) {
+        LOGGER.debug("Validate APR rate is: " + expectedAPRrate);
+        String actualReaAPRrate = ActOn.elements(driver, ActualAPRRate).getTextValue();
+        Assert.assertEquals(expectedAPRrate, actualReaAPRrate);
+        return this;
+    }
+}
---
+++ src/test/java/step_definitions/MortgageCalculatorSteps.java
@@ -0,0 +1,57 @@
+package step_definitions;
+
+import command_providers.ActOn;
+import io.cucumber.datatable.DataTable;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.WebDriver;
+import page_objects.Home;
+import page_objects.RealAPR;
+import utilities.ReadConfigFiles;
+
+import java.util.List;
+import java.util.Map;
+
+public class MortgageCalculatorSteps {
+    private static final Logger LOGGER = LogManager.getLogger(MortgageCalculatorSteps.class);
+    WebDriver driver = Hooks.driver;
+
+    @Given("^user is in mortgage calculator home page$")
+    public void navigateToMortgageCalculatorHomePage() {
+        ActOn.browser(driver).openBrowser(ReadConfigFiles.getPropertyValues("MortgageURL"));
+        LOGGER.info("Landed on the Mortgage Calculator Home Page");
+     }
+
+    @And("^user navigates to the Real APR page$")
+    public void userNavigatesToRealAprPage() {
+        new Home(driver)
+                .mouseHoverToRates()
+                .navigateToRealApr();
+        LOGGER.info("Navigate to the Real APR page");
+    }
+
+    @When("^user clicks on the Calculate button upon entering data$")
+    public void clickOnCalculateButtonUponEnteringData(DataTable table) {
+        List<Map<String, String>> data = table.asMaps(String.class, String.class);
+        for (Map<String, String> cells: data) {
+            new RealAPR(driver)
+                    .typeHousePrice(cells.get("HomePrice"))
+                    .clickOnDownPaymentInDollar()
+                    .typeDownPayment(cells.get("DownPayment"))
+                    .typeInterestRate(cells.get("InterestRate"))
+                    .clickOnTheCalculateButton();
+            LOGGER.info("Entering data to calculate Real APR");
+        }
+    }
+
+    @Then("^the Real APR is \"(.+?)\"$")
+    public void the_real_apr_is(String realApr) {
+        new RealAPR(driver)
+                .validateActualRealAPR(realApr);
+        LOGGER.info("Validating the Real APR");
+    }
+}
